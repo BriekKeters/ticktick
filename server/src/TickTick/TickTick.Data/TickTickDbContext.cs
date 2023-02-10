1b18cc107664217fa1db54e704289af37f28a624
BriekKeters <briekk01@gmail.com>
2023-02-10 13:34:31 +0100

    Db setup

---
+++ server/src/TickTick/TickTick.Data/TickTickDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TickTick.Models;
+
+namespace TickTick.Data;
+
+public class TickTickDbContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+
+
+    public TickTickDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+}
+
